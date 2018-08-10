5ecbafdce1c3b1735d5783d794ac7bfde8e2a738
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-08-10 12:18:30 +1000

    Added simple formatting for Big5 traits

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './PersonalityInsights.svg';
 import './App.css';
+//import helper from './helper';
 
 class App extends Component {
 
@@ -33,12 +34,41 @@ class App extends Component {
           <h1 className="App-title">Watson Personality</h1>
           <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <em>The following is the results of a your personality test based on the supplied text.<br/>
+          The percentage indicates the percentile you exhibit for that trait. For example if<br />
+          you have a 65% in a trait, you are higher than 65% of people and lower than 35%.
+          </em>
         <p className="App-intro">
-          {this.state.response ? JSON.stringify(this.state.response, null, 4) : 'Waiting for response...'}
+          {this.state.response ? BigFivePersonality(this.state.response.personality) : 'Waiting for response...'}
         </p>
       </div>
     );
   }
+}
+
+const BigFivePersonality = (props) => {
+  return(
+    <div>
+    {
+      props.map(trait => {
+        return(
+          <div>
+          <h1>{trait.name} ({Math.round(trait.percentile * 100)}%)</h1>
+          <ul>
+          {
+            trait.children.map(t => {
+              return(
+                <li>{t.name} ({Math.round(t.percentile * 100)}%)</li>
+              )
+          })
+        }
+        </ul>
+        </div>
+        )
+
+    })
+  }</div>
+  );
 }
 
 export default App;
---
+++ client/src/helper.js
@@ -0,0 +1,3 @@
+exports.CapitalizeFirstLetter = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
