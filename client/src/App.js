3d7a25c4694ee2b71122b6cfa0bfa8214b6e140f
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-08-11 15:16:43 +1000

    Displayed remaining personality statistics

--- client/src/App.js
+++ client/src/App.js
@@ -34,41 +34,93 @@ class App extends Component {
           <h1 className="App-title">Watson Personality</h1>
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <em>The following is the results of a your personality test based on the supplied text.<br/>
+        <p>The following is the results of a your personality test based on the supplied text.<br/>
           The percentage indicates the percentile you exhibit for that trait. For example if<br />
           you have a 65% in a trait, you are higher than 65% of people and lower than 35%.
-          </em>
+          </p>
         <p className="App-intro">
-          {this.state.response ? BigFivePersonality(this.state.response.personality) : 'Waiting for response...'}
+          {this.state.response ? DisplayBigFive(this.state.response.personality) : 'Waiting for response...'}
+          {this.state.response ? DisplayNeeds(this.state.response.needs) : ''}
+          {this.state.response ? DisplayValues(this.state.response.values) : ''}
+          {this.state.response ? DisplayConsumptionPreferences(this.state.response.consumption_preferences) : ''}
         </p>
       </div>
     );
   }
 }
 
-const BigFivePersonality = (props) => {
+const DisplayBigFive = (personality) => {
   return(
     <div>
+      <h1>Big 5</h1>
+      <p>Many contemporary personality psychologists believe that there are five basic<br />
+          dimensions of personality, often referred to as the "Big 5" personality traits.<br/>
+          The five broad personality traits described by the theory are<br/>
+          extraversion, agreeableness, openness, conscientiousness, and neuroticism.
+      </p>
     {
-      props.map(trait => {
+      personality.map(trait => {
         return(
           <div>
-          <h1>{trait.name} ({Math.round(trait.percentile * 100)}%)</h1>
-          <ul>
-          {
-            trait.children.map(t => {
-              return(
-                <li>{t.name} ({Math.round(t.percentile * 100)}%)</li>
-              )
-          })
-        }
-        </ul>
+          <h2>{trait.name} ({Math.round(trait.percentile * 100)}%)</h2>
+          {MapItemPercentile(trait.children)}
         </div>
         )
 
     })
   }</div>
   );
+}
+
+const DisplayNeeds = (needs) => {
+  return(
+    <div>
+      <h1>Needs</h1>
+      <p>This section describes the needs of the person inferred from the given text.
+      </p>
+    {MapItemPercentile(needs)}
+    </div>
+  );
+}
+
+const DisplayValues = (values) => {
+  return(
+    <div>
+      <h1>Values</h1>
+      <p>This section describes the most valued traits or ideals inferred from the text.
+      </p>
+    {MapItemPercentile(values)}
+    </div>
+  );
+}
+
+const DisplayConsumptionPreferences = (preferences) => {
+  return(
+    <div>
+      <h1>Consumption preferences</h1>
+      <p>Consumption preferences are scored 0, 50 or 100 rather than weighted as a percentile.</p>
+    {
+      preferences.map(category => {
+        return(
+          <div>
+          <h2>{category.name}</h2>
+          {MapItemPercentile(category.consumption_preferences, 'score')}
+        </div>
+        )
+
+    })
+  }</div>
+  );
+}
+
+function MapItemPercentile(arr, childName = 'percentile') {
+  return arr.map(item => {
+    return(
+      <ul>
+      <li>{item.name} ({Math.round(item[childName] * 100)}%)</li>
+    </ul>
+    )
+})
 }
 
 export default App;
